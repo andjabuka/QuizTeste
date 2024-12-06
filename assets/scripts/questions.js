e495ef9392fd6cb0793e40c380eed67ac2025f42
Anderson <andslipknoty@gmail.com>
2024-12-06 15:02:38 -0300

    atualização js

---
+++ assets/scripts/questions.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+        numb: 1,
+        question:"Qual das opções abaixo é usada para manipular elementos HTML no DOM?",
+        answer:"a) document.querySelector()",
+        options: [
+            "a) document.querySelector()",
+            "b) console.log()",
+            "c) alert()",
+            "d) JSON.stringify()"
+
+        ]
+    },
+    {
+        numb: 2,
+        question:"O que acontece quando você usa event.preventDefault() em um evento?",
+        answer:"b) O comportamento padrão do evento é interrompido.",
+        options: [
+            "a) A página é recarregada automaticamente.",
+            "b) O comportamento padrão do evento é interrompido.",
+            "c) O evento é cancelado sem acionar a função associada.",
+            "d) A execução da função para imediatamente."
+        ]
+    },
+    {
+        numb: 3,
+        question:"Qual método JavaScript é usado para adicionar um evento a um elemento HTML?",
+        answer:"c) element.addEventListener()",
+        options: [
+            "a) element.addEvent()",
+            "b) element.attachEvent()",
+            "c) element.addEventListener()",
+            "d) element.setEvent()"
+        ]
+    },
+    {
+        numb: 4,
+        question:"Qual das opções abaixo é usada para transformar uma NodeList em um array em JavaScript?",
+        answer:"b) Array.from()",
+        options: [
+            "a) Array.nodeList()",
+            "b) Array.from()",
+            "c) nodeList.toArray()",
+            "d) NodeList.map()"
+        ]
+    },
+    {
+        numb: 5,
+        question:"Qual declaração em JavaScript pode ser usada para criar uma variável que não pode ser reatribuída?",
+        answer:"c) const",
+        options: [
+            "a) let",
+            "b) var",
+            "c) const",
+            "d) function"
+        ]
+    },
+
+];
